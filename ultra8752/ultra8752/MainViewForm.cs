f7f20f489086467177ac48df1f402e40b3f36c1d
DarinX64 <darin.x64@gmail.com>
2020-10-31 11:28:19 +0200

    Added form to display

---
+++ ultra8752/ultra8752/MainViewForm.cs
@@ -0,0 +1,48 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ultra8752
+{
+    public partial class MainViewForm : Form
+    {
+        public MainViewForm(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length < 1)
+            {
+                Console.Out.WriteLine("No input file provided. Usage: ultra8752 <file>");
+                System.Environment.Exit(-1);
+            }
+
+            if (!System.IO.File.Exists(args[0]))
+            {
+                Console.Out.WriteLine("Invalid input file path");
+                System.Environment.Exit(-1);
+            }
+
+            VideoLoader videoLoader = new VideoLoader(args[0]);
+            Mat frame;
+            for (; ; )
+            {
+                frame = videoLoader.nextFrame();
+                if (frame == null)
+                {
+                    return;
+                }
+
+                Mat processed = ImageProcessor.ProcessImage(frame);
+
+                pictureBox1.Image = processed.ToBitmap();
+            }
+        }
+    }
+}
--- ultra8752/ultra8752/Program.cs
+++ ultra8752/ultra8752/Program.cs
@@ -22,36 +22,7 @@ namespace ultra8752
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            if (args.Length < 1)
-            {
-                Console.Out.WriteLine("No input file provided. Usage: ultra8752 <file>");
-                System.Environment.Exit(-1);
-            }
-
-            if (!System.IO.File.Exists(args[0]))
-            {
-                Console.Out.WriteLine("Invalid input file path");
-                System.Environment.Exit(-1);
-            }
-
-            VideoLoader videoLoader = new VideoLoader(args[0]);
-            MainView mainView = new MainView();
-            Mat frame;
-            for (; ; )
-            {
-                frame = videoLoader.nextFrame();
-                if(frame == null)
-                {
-                    return;
-                }
-
-                Mat processed = ImageProcessor.ProcessImage(frame);
-
-                mainView.DisplayImage(processed);
-
-                // TODO: display processed
-            }
+            Application.Run(new MainViewForm(args));
         }
     }
 }
